da46b8c29ebf2e2b67abc2dac08a49d7a90a802f
Anurag Pandey <108535343+anurag-p15@users.noreply.github.com>
2023-08-25 16:37:42 +0530

    Home page demo done

--- quizfiesta/src/components_home/home.js
+++ quizfiesta/src/components_home/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './navbar';
+import Body from './body';
+import Footer from './footer';
 
 function Home(){
     return(
     <div>
         <Navbar/>
+        <Body/>
+        <Footer/>
     </div>
     )
 }
