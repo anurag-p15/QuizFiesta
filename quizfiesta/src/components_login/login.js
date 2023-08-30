199726685e038e9938203a782db8e9e6a747dccc
Anurag Pandey <108535343+anurag-p15@users.noreply.github.com>
2023-08-30 16:10:00 +0530

    Text upgrade

--- quizfiesta/src/components_login/login.js
+++ quizfiesta/src/components_login/login.js
@@ -1,7 +1,7 @@
 function LoginForm () {
     return (
-        <div className="row" style={{ margin: '5% 2%'}}>
-        <div className="col-lg-6 mx-auto p-7">
+      <div className="row">
+         <div className="col-lg-6 mx-auto p-7">
           <div className="card" style={{ margin: '2% 5%',borderRadius:'3vh' }}>
             <div className="login-box">
               <div className="login-snip">
@@ -61,28 +61,7 @@ function LoginForm () {
             </div>
           </div>
         </div>
-        <div className="col-lg-6 mx-auto px-5">
-          <div className="text-login" style={{ marginLeft: '5%',marginRight:'5%'}}>
-            <div>
-              <div>
-                <p>Login/Register</p>
-              </div>
-              <div>
-                <p style={{ textAlign: 'left' }}>Ready to join the QuizFiesta community? Create your account now and unlock a realm of knowledge
-                 and fun. With a simple sign-up, you'll gain access to a variety of engaging quizzes that spark curiosity and enhance your 
-                 learning experience. Let's embark on this educational adventure together – sign up today and let the quizzing begin!</p>
-                <p style={{ textAlign: 'center' }}>Already a Registered User? </p><p style={{textAlign:'left'}}>Log in to your account to dive into a world of captivating quizzes, challenge your friends, 
-                and expand your knowledge horizons. Your next quiz adventure awaits!</p>
-                <p style={{textAlign:'left'}}>Your Privacy Matters to Us! Your login information is safe and secure with QuizFiesta. We value your trust and want to assure
-                   you that your personal details are never shared. Logging in not only ensures the safety of your account but also enhances your 
-                   in-app experience, allowing you to explore a world of knowledge and fun seamlessly. Join us with confidence, and let the 
-                   quizzing adventure begin!</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
+      </div>     
     );
 }
 
